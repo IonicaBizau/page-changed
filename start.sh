0e8907b2f8112484ad3658189a7eb98676fc83e2
Ionică Bizău <bizauionica@yahoo.com>
2014-04-17 16:31:38 +0300

    Added page-changed

--- start.sh
+++ start.sh
@@ -1,44 +1,13 @@
 #!/bin/bash
 
-#############################
-# Check Internet Connectivity
-#############################
-# A small script that plays a beep when there is no Internet connection
-#
-# Follow me on Github :: github.com/IonicaBizau
-# ==========================
-LOGFILE=$HOME'/internet-connection.log'
+oldHtml=""
+newHtml=""
 
-if ! which mplayer > /dev/null; then
-  echo "Mplayer not found. Please install it by typing sudo apt-get install mplayer"
-  exit 1;
-fi
-
-function check {
-  ping -w 1 -c 1 $1;
-}
-
-function play {
-  $1 beep-1.wav 2>/dev/null 1>>$LOGFILE &
-}
-
-hosts=(`ip route | grep default | awk '{print $3}'` "google.com" "stackoverflow.com")
-
-echo "> Daemon started at `date +%d/%m/%Y' '%T`" >> $LOGFILE
-
-for (( ; ; )); do
-  for item in "${hosts[@]}"; do
-
-    echo " * Checking $item" >> $LOGFILE
-
-    if check $item > /dev/null; then
-      echo "Connection is up" >> $LOGFILE
-    else
-      echo "+-----------------------------------------------------------------+" >> $LOGFILE
-      echo "+    !!!!!    No Internet connection - $item    !!!!!   +" >> $LOGFILE
-      echo "+-----------------------------------------------------------------+" >> $LOGFILE
-      play mplayer
+while true; do
+    newHtml=idleTimeMillis=`curl $1`
+    if [ "$newHtml" != "$oldHtml" ]
+    then
+        oldHtml=$newHtml;
+        play ./beep.wav
     fi
-  done
-  sleep 10
 done
